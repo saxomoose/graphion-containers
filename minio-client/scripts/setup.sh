a8299b6b08ead545e90afeb284f4259068fd59e0
saxomoose <mathieu.tulpinck@gmail.com>
2023-06-19 10:32:40 +0000

    Cleanup

--- minio-client/scripts/setup.sh
+++ minio-client/scripts/setup.sh
@@ -4,7 +4,10 @@ if ! mc alias ls | grep -q "$MINIO_HOST"; then
   mc alias set $MINIO_HOST http://$MINIO_HOST:$MINIO_PORT $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
   mc admin user add $MINIO_HOST $MINIO_UNPRIV_USER $MINIO_UNPRIV_PASSWORD
   mc admin policy attach $MINIO_HOST readwrite --user $MINIO_UNPRIV_USER
-  mc mb --ignore-existing $MINIO_HOST/$MINIO_BUCKET
+  mc mb --ignore-existing $MINIO_HOST/$MINIO_ADMIN_BUCKET
+  mc mb --ignore-existing $MINIO_HOST/$MINIO_TEST_BUCKET
+  mc cp --recursive ./certs/ $MINIO_HOST/$MINIO_ADMIN_BUCKET/certs
+
 else
   echo "Server already initialized."
 fi
--- notes.md
+++ notes.md
@@ -1,3 +1,2 @@
 docker compose -f docker-compose-test.yml up -d
-
 docker compose -f docker-compose-test.yml down --volumes
